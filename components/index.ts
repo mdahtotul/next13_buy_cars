3e7fce3b00202505a974a296ed1c89919fe74f9c
mdahtotul <mdahtotul@gmail.com>
2023-07-20 03:23:59 +0600

    search manufacturer initial phase done

--- components/index.ts
+++ components/index.ts
@@ -1,6 +1,17 @@
 import CustomButton from "@/rootComp/CustomButton";
+import CustomFilter from "@/rootComp/CustomFilter";
 import Footer from "@/rootComp/Footer";
 import Hero from "@/rootComp/Hero";
 import Navbar from "@/rootComp/Navbar";
+import SearchBar from "@/rootComp/SearchBar";
+import SearchManufacturer from "@/rootComp/SearchManufacturer";
 
-export { CustomButton, Footer, Hero, Navbar };
+export {
+  CustomButton,
+  CustomFilter,
+  Footer,
+  Hero,
+  Navbar,
+  SearchBar,
+  SearchManufacturer,
+};
