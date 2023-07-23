9e6f8f8b3da225a73e183d6a2b65559e1af8722b
mdahtotul <mdahtotul@gmail.com>
2023-07-23 17:30:04 +0600

    searchbar ui

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,10 +1,30 @@
 "use client";
 
+import Image from "next/image";
 import { FC, useState } from "react";
 import SearchManufacturer from "./SearchManufacturer";
 
+interface SearchButtonProps {
+  otherClasses?: string;
+}
+
+const SearchButton: FC<SearchButtonProps> = ({ otherClasses }) => {
+  return (
+    <button type="submit" className={`z-10 ml-3 ${otherClasses}`}>
+      <Image
+        src="/magnifying-glass.svg"
+        alt="magnifying glass"
+        width={40}
+        height={40}
+        className="object-contain"
+      />
+    </button>
+  );
+};
+
 const SearchBar: FC = () => {
-  const [searchVal, setSearchVal] = useState("");
+  const [manufacturerVal, setManufacturerVal] = useState("");
+  const [modelVal, setModelVal] = useState("");
 
   const handleSearch = (): void => {};
 
@@ -12,10 +32,33 @@ const SearchBar: FC = () => {
     <form className="searchbar" onSubmit={handleSearch}>
       <div className="searchbar__item">
         <SearchManufacturer
-          manufacturer={searchVal}
-          setManufacturer={setSearchVal}
+          manufacturer={manufacturerVal}
+          setManufacturer={setManufacturerVal}
         />
+        <SearchButton otherClasses="sm:hidden" />
       </div>
+
+      <div className="searchbar__item">
+        <Image
+          src="/model-icon.png"
+          alt="car model"
+          height={25}
+          width={25}
+          className="absolute ml-4 h-[20px] w-[20px]"
+        />
+
+        <input
+          type="text"
+          name="model"
+          value={modelVal}
+          onChange={(e) => setModelVal(e.target.value)}
+          placeholder="Tiguan"
+          className="searchbar__input"
+        />
+
+        <SearchButton otherClasses="sm:hidden" />
+      </div>
+      <SearchButton otherClasses="max-sm:hidden" />
     </form>
   );
 };
